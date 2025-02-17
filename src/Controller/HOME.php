c2353e6a737cc9f86984e30e7be0bf268710f4a9
Sahar <Sahar.OUJIBOUGHANMI@esprit.tn>
2025-02-17 10:45:55 +0100

    Initial work on concours

---
+++ src/Controller/HOME.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ConcoursRepository;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HOME extends AbstractController
+{
+    #[Route('/home', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('back/base.html.twig', [
+            'home' => 'home',
+        ]);
+    }
+
+    #[Route('/home1', name: 'home1')]
+    public function index1(): Response
+    {
+        return $this->render('front1/base.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+    #[Route('/home2', name: 'home2')]
+    public function index2(): Response
+    {
+        return $this->render('front1/test.html.twig', [
+            'test' => 'test',
+        ]);
+    }
+    #[Route('/home3', name: 'home3')]
+    public function index3(): Response
+    {
+        return $this->render('back/test.html.twig', [
+            'test' => 'test',
+        ]);
+    }
+
+    #[Route('/h1', name: 'h1')]
+    public function index31(ConcoursRepository $concoursRepository): Response
+    {
+        return $this->render('concours/affiche.html.twig', [
+            'concours' => $concoursRepository->findAll(),
+        ]);
+    }
+}
